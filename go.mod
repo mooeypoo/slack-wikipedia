13e52657e134fc263cb60e6e1e875aeb1b7c4551
Moriel Schottlender <moriel@gmail.com>
2020-05-26 02:16:27 -0700

    Changing to slacker library

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/sbstjn/allot v0.0.0-20161025071122-1f2349af5ccd // indirect
 	github.com/sbstjn/hanu v0.1.0
+	github.com/shomali11/slacker v0.0.0-20200420173605-4887ab8127b6
 	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2 // indirect
 )
--- main.go
+++ main.go
@@ -1,38 +1,69 @@
 package main
 
 import (
+	"context"
+	"github.com/shomali11/slacker"
 	"log"
 	"os"
-	"strings"
-
-	"github.com/sbstjn/hanu"
 )
 
 func main() {
 	token := os.Getenv("SLACK_TOKEN")
+	bot := slacker.NewClient(token)
 
-	// slack, err := hanu.New("xoxb-1143242370325-1144948666467-D7affrVWkgWE2o3fghcPcYnq")
-	slack, err := hanu.New(token)
+	definition := &slacker.CommandDefinition{
+		Description: "Repeat the text given.",
+		Example:     "repeat foo bar baz",
+		Handler: func(request slacker.Request, response slacker.ResponseWriter) {
+			text := request.StringParam("text", "")
+			response.Reply(text)
+		},
+	}
 
+	bot.Command("echo <text>", definition)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	Version := "0.0.1"
-
-	slack.Command("shout <word>", func(conv hanu.ConversationInterface) {
-		str, _ := conv.String("word")
-		conv.Reply(strings.ToUpper(str))
-	})
-
-	slack.Command("whisper <word>", func(conv hanu.ConversationInterface) {
-		str, _ := conv.String("word")
-		conv.Reply(strings.ToLower(str))
-	})
-
-	slack.Command("version", func(conv hanu.ConversationInterface) {
-		conv.Reply("Thanks for asking! I'm running `%s`", Version)
-	})
-
-	slack.Listen()
 }
+
+// package main
+
+// import (
+// 	"log"
+// 	"os"
+// 	"strings"
+
+// 	"github.com/sbstjn/hanu"
+// )
+
+// func main() {
+// 	token := os.Getenv("SLACK_TOKEN")
+// 	slack, err := hanu.New(token)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	Version := "0.0.1"
+
+// 	slack.Command("shout <word>", func(conv hanu.ConversationInterface) {
+// 		str, _ := conv.String("word")
+// 		conv.Reply(strings.ToUpper(str))
+// 	})
+
+// 	slack.Command("whisper <word>", func(conv hanu.ConversationInterface) {
+// 		str, _ := conv.String("word")
+// 		conv.Reply(strings.ToLower(str))
+// 	})
+
+// 	slack.Command("version", func(conv hanu.ConversationInterface) {
+// 		conv.Reply("Thanks for asking! I'm running `%s`", Version)
+// 	})
+
+// 	slack.Listen()
+// }
